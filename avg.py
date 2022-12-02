d139df56cad4471a190a2300b7495b315d2d9186
Ankit Kumar Roy <ankitkumar98roy@gmail.com>
2022-12-02 23:44:35 +0530

    pro

---
+++ avg.py
@@ -0,0 +1,11 @@
+
+# x=int(input("enter your digit="))
+# y=int(input("enter your snd digit="))
+# z=int(input("enter your thrd digit="))
+# a=(x+y+z)/3
+
+# print(a)
+# print(f"hello your first no{x} second no {y} thrd no {z} avg {a}")
+x,y,z=input("Enter your no=").split(",")
+
+print(f"yor average{(int(x)+int(y)+int(z))/3}")
