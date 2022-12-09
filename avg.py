aef7b0255b99a5ff82b40e9e4133e657bc65c86e
Ankit Kumar Roy <ankitkumar98roy@gmail.com>
2022-12-09 16:17:18 +0530

    new

--- avg.py
+++ avg.py
@@ -6,6 +6,6 @@
 
 # print(a)
 # print(f"hello your first no{x} second no {y} thrd no {z} avg {a}")
-x,y,z=input("Enter your no=").split(",")
+# x,y,z=input("Enter your no=").split(",")
 
-print(f"yor average{(int(x)+int(y)+int(z))/3}")
+# print(f"yor average{(int(x)+int(y)+int(z))/3}")
