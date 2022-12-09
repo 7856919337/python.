2965df9ae236173e4c5ded6d571098324fe25193
Ankit Kumar Roy <ankitkumar98roy@gmail.com>
2022-12-09 16:19:43 +0530

    work

---
+++ dict.py
@@ -0,0 +1,28 @@
+# mydictionary= {
+#     "name":"Ankit Kumar Roy",
+#     "age": 19,
+#     "course":"btech"
+# }
+# a=mydictionary["name"]
+
+# a=mydictionary.keys()
+# a=mydictionary.items()
+# mydictionary["age"]=44
+# a=mydictionary.keys()
+# b=mydictionary.values()
+# print(a)
+# print(b)
+# myDictionary = {
+#     "class":{
+#         "student":{
+#             "name" : "abc",
+#             "marks" : {
+#                 "python" : 90,
+#                 "web" : 95
+#             }
+#         }
+#     }
+# }
+# print(myDictionary["class"]["student"]["marks"]["web"])
+
+
